74bc94c30615ea11fbd6bdb257ad114cb453d411
POS <prideofshadows@gmail.com>
2025-08-06 21:48:05 +0300

    Finished second chapter of the tutorial

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # this allows us to use code from the open-source pygame library throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()  # Initialize all imported pygame modules
@@ -8,12 +9,28 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}") 
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while(1):
+    clock = pygame.time.Clock()  # Create a clock object to manage frame rate
+    dt=0  # Initialize delta time variable
+
+    updateable = pygame.sprite.Group()  # Create a group for updateable sprites
+    drawable = pygame.sprite.Group()  # Create a group for drawable sprites
+    
+    Player.containers = (updateable, drawable)  # Add player to both groups
+
+    # Create a player instance at the center of the screen
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
+            
+        screen.fill("black")
+        for draw in drawable:
+            draw.draw(screen)  # Draw all drawable sprites
         pygame.display.flip()  # Update the full display Surface to the screen
+        dt = clock.tick(60 )/ 1000 # Limit the frame rate to 60 FPS
+        updateable.update(dt)
 
 
 if __name__ == "__main__":
